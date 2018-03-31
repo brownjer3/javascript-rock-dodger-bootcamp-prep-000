1190d25ecf4fde7bcc5d67859822bb463f5a3413
Jerry Brown <brownjer3@gmail.com>
2018-03-31 18:37:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ function createRock(x) {
      * If a rock collides with the DODGER,
      * we should call endGame()
      */
-     if(checkCollision(rock) === true) {
+     if(checkCollision(rock)) {
        endGame();
      }
 
