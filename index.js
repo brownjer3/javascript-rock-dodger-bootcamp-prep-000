993d856c8a43c593d5d2279f6896ba5f82a50b7a
Jerry Brown <brownjer3@gmail.com>
2018-03-31 19:24:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -151,7 +151,7 @@ function moveDodger(e) {
 
 function moveDodgerLeft() {
   window.requestAnimationFrame(function() {
-    
+    let left = positionToInteger(DODGER.style.left);
   }
 
   // implement me!
