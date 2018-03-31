e522cdcf10220c2f5e504bfd614db3d60c8ff0aa
Jerry Brown <brownjer3@gmail.com>
2018-03-31 18:57:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -126,7 +126,7 @@ function createRock(x) {
 function endGame() {
   clearInterval(gameInterval);
   for(let i = 0; i <= ROCKS.length; i++) {
-
+    ROCKS[i].remove();
   }
   window.removeEventListener('keydown', moveDodger);
   window.alert("YOU LOSE!");
