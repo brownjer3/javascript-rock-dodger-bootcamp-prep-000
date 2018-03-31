0b00a65be9feaa4c50519dee51cf049e9e7f8266
Jerry Brown <brownjer3@gmail.com>
2018-03-31 19:40:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -158,6 +158,8 @@ function moveDodgerLeft() {
     const left = positionToInteger(DODGER.style.left);
     if(left > 0) {
       DODGER.style.left = '${left - 4}px';
+    } else {
+      return
     }
   });
 
