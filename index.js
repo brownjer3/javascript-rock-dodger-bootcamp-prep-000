a9d3970bc2328620a7656fbb9b7f31886601ca28
Jerry Brown <brownjer3@gmail.com>
2018-03-31 19:09:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -128,7 +128,7 @@ function endGame() {
   for(let i = 0; i < ROCKS.length; i++) {
     ROCKS[i].remove();
   }
-  document.removeEventListener(`keydown`, moveDodger);
+  window.removeEventListener('keydown', moveDodger);
   return alert('YOU LOSE!');
 }
 
