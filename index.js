90ced43dcdf576f757fca188dec2d01bf3cf6367
Jerry Brown <brownjer3@gmail.com>
2018-03-31 19:32:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -135,6 +135,7 @@ function endGame() {
 function moveDodger(e) {
   if(e.which === LEFT_ARROW) {
     moveDodgerLeft();
+    e.stopPropagation();
   } else if (e.which === RIGHT_ARROW) {
     moveDodgerRight();
   }
